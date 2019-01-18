91c1fa689a7dcf678faf7fb4e0c43266622d2ac9
Christophe Pirolley <christophe.pirolley@fr.clara.net>
2019-01-18 11:49:40 +0100

    add param

--- manifests/php.pp
+++ manifests/php.pp
@@ -55,6 +55,7 @@ define newrelic::php (
   $newrelic_ini_transaction_tracer_slow_sql              = undef,
   $newrelic_ini_transaction_tracer_stack_trace_threshold = undef,
   $newrelic_ini_transaction_tracer_threshold             = undef,
+  $newrelic_ini_distributed_tracing_enabled              = undef,
   $newrelic_ini_capture_params                           = undef,
   $newrelic_ini_ignored_params                           = undef,
   $newrelic_daemon_cfgfile_ensure                        = 'present',
